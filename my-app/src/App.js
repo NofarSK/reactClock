953d9352096450a5f0b6821f589257e736e63783
NofarSK <nofar1608@gmail.com>
2024-06-01 23:14:34 +0300

    update

---
+++ my-app/src/App.js
@@ -0,0 +1,27 @@
+
+
+import Expenses from "./components/Expenses";
+import ExpenseForm from "./components/ExpenseForm";
+
+function App() {
+
+  const expense = [
+    { title: 'mazda', amount: 3000, date: new Date(2020, 2, 28) },
+    { title: 'ford', amount: 2000, date: new Date(2021, 3, 12) },
+    { title: 'nisan', amount: 1000, date: new Date(2022, 4, 8) },
+    { title: 'honda', amount: 500, date: new Date(2023, 5, 20) },
+
+  ];
+  return (
+    <div>
+
+      <ExpenseForm />
+
+      <Expenses items={expense} />
+
+    </div>
+
+  );
+}
+
+export default App;
